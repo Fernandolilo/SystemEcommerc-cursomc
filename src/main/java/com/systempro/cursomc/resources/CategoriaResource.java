55b69b5ce4792a1969ac403bb175a434b73a6551
Fernandolilo <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-03-12 20:44:55 -0300

    Criado banco de Dados H2 e criação automatica da base de dados

--- src/main/java/com/systempro/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/systempro/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,28 @@
 package com.systempro.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.systempro.cursomc.domain.Categoria;
 
 // class de controle REST
 @RestController
-@RequestMapping(value ="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "O REST esta funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
+
 }
