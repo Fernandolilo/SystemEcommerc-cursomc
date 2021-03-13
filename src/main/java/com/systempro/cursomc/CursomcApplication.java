03cf17a4412dd85fd4dd6254e04ad8abdf7de052
Fernandolilo <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-03-12 21:45:02 -0300

    Criando operação de estanciação

--- src/main/java/com/systempro/cursomc/CursomcApplication.java
+++ src/main/java/com/systempro/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.systempro.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.systempro.cursomc.domain.Categoria;
+import com.systempro.cursomc.domain.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+	
+	@Autowired
+	private CategoriaRepository categoriaRepositori;
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepositori.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
+
 
 }
