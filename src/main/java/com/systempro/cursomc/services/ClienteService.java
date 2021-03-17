3128e78c275533f068889f1775c05b312156715a
Fernandolilo <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-03-16 23:05:08 -0300

    Criado endpoint clientes/{id}

---
+++ src/main/java/com/systempro/cursomc/services/ClienteService.java
@@ -0,0 +1,28 @@
+package com.systempro.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.systempro.cursomc.domain.Cliente;
+import com.systempro.cursomc.domain.repositories.ClienteRepository;
+import com.systempro.cursomc.services.excepitions.ObjectNotFoundException;
+
+
+
+//implementa o tratamento do serviço
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find(Integer id) { 
+		 Optional<Cliente> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException( 
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())); 
+		} 
+
+
+}
