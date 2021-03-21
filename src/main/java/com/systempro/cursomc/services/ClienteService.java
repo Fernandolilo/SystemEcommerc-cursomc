eda972bc9b4d2a29cc8a73bb197cc79e5abfbf1a
Fernandolilo <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-03-20 23:14:31 -0300

    PUT, DELETE, GET em Clientes

--- src/main/java/com/systempro/cursomc/services/ClienteService.java
+++ src/main/java/com/systempro/cursomc/services/ClienteService.java
@@ -1,28 +1,64 @@
 package com.systempro.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.systempro.cursomc.domain.Cliente;
+import com.systempro.cursomc.domain.dto.ClienteDTO;
 import com.systempro.cursomc.domain.repositories.ClienteRepository;
+import com.systempro.cursomc.services.excepitions.DataIntegrityException;
 import com.systempro.cursomc.services.excepitions.ObjectNotFoundException;
-
-
 
 //implementa o tratamento do serviço
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private ClienteRepository repo;
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível exclui uma categoria que possui produtos ");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 	
-	public Cliente find(Integer id) { 
-		 Optional<Cliente> obj = repo.findById(id); 
-		return obj.orElseThrow(() -> new ObjectNotFoundException( 
-		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())); 
-		} 
-
-
 }
