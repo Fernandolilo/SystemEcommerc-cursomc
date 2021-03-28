b17bfe117ac8810926cf923829ef782f04cdf414
Fernandolilo <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-03-27 22:43:05 -0300

    Busca de produtos pelo nome e categoria

--- src/main/java/com/systempro/cursomc/domain/repositories/ProdutoRepository.java
+++ src/main/java/com/systempro/cursomc/domain/repositories/ProdutoRepository.java
@@ -1,12 +1,26 @@
 package com.systempro.cursomc.domain.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.systempro.cursomc.domain.Categoria;
 import com.systempro.cursomc.domain.Produto;
 
 //interfaca de acesso a dados
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	// duas formas de fazer uma busca relacionando tabelas, uma usando o jpql e outra usando o Spring
+	
+	//@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	//Page<Produto>search(@Param("nome") String nome, @Param("categorias")List<Categoria> categorias, Pageable pageRequest);
+	@Transactional(readOnly = true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto>findDistinctByNomeContainingAndCategoriasIn( String nome, List<Categoria> categorias, Pageable pageRequest);
 }
